c68992bd7f238f1d478aa54cf4a229cdf40d5432
우빈 <102154880+Ubinquitous@users.noreply.github.com>
2022-07-07 16:04:51 +0900

    SET :: HIGHLOW 구현

--- HIGHLOW/script.js
+++ HIGHLOW/script.js
@@ -4,65 +4,224 @@ const card2 = document.querySelector("#card2");
 const bettingForm = document.querySelector("#betting");
 const upBtn = document.getElementById("upBetting");
 const downBtn = document.getElementById("downBetting");
+const calcBetHigh = document.getElementById("calcBetHigh");
+const calcBetLow = document.getElementById("calcBetLow");
+const betMoney = document.getElementById("betMoney");
+const bettingButton = document.querySelector(".bettingButton");
+const upRange = document.getElementById("reward");
+const downRange = document.getElementById("Dreward");
 
 let card1Num, card2Num;
 let card1Type, card2Type;
-
+let highMoneyPer, lowMoneyPer;
 let money = 10000;
 h2.innerText = money;
 
 const cardType = ["H_", "D_", "S_", "C_"];
-const cardNum = ["JOKER", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+const cardNum = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", "JOKER"];
 // 사진으로 대체 예정.
+card2Type = cardType[Math.round(Math.random()*3)];
+card2Num  = cardNum[Math.round(Math.random()*13)];
 
 createStandardCard();
+calculatorMoney();
+
+console.log(`${card2Num}`);
 
 bettingForm.addEventListener("submit", bettingFormPrevent);
 upBtn.addEventListener("click", ifButtonClickIsUp);
-//downBtn.addEventListener("click", ifButtonClickIsDown);
-
+downBtn.addEventListener("click", ifButtonClickIsDown);
 
 function bettingFormPrevent(event){
     event.preventDefault();
 }
-
+// 이미지 createElement하기
 function createStandardCard(){
     card1Type = cardType[Math.round(Math.random()*3)];
-    card1Num  = cardNum[Math.round(Math.random()*14)];
-    card1.innerText = `${card1Type}${card1Num}`;
+    card1Num  = cardNum[Math.round(Math.random()*13)];
+
+    if(card1Num==="1" || card1Num==="JOKER"){
+        card1Num="7";
+    } 
+
+    //card1.innerText = `${card1Type}${card1Num}`;
 }
 
 function ifButtonClickIsUp(){
-    card2Type = cardType[Math.round(Math.random()*3)];
-    card2Num  = cardNum[Math.round(Math.random()*14)];
+    bettingButton.classList.add("hidden");
+    
+    if(card2Num==="JOKER" && card1Num !== "JOKER"){
+        money = money + parseInt(betMoney.value*highMoneyPer);
+        h2.innerText = `${money}`;
+    }
+    if(card2Num==="JOKER" && card1Num === "JOKER"){
+        console.log(`draw`);
+    }
+    if(card2Num!=="JOKER" && card1Num === "JOKER"){
+        money = money - parseInt(betMoney.value);
+        h2.innerText = `${money}`;
+    }
 
-    console.log(`${card1Num}, ${card2Num}`);
 
     if(isNaN(parseInt(card2Num)) && card2Num!=="JOKER" && isNaN(parseInt(card1Num)) && card1Num!=="JOKER"){
         if((card1Num==="J" && card2Num==="Q") || (card1Num==="Q" && card2Num==="K") || (card1Num==="K" && card2Num==="A") || (card1Num==="Q" && card2Num==="A")){
-            console.log(`card2Num`);
-            money*=2;
+            money = money + parseInt(betMoney.value*highMoneyPer);
             h2.innerText = `${money}`;
         } else if(card1Num===card2Num){
             console.log(`draw`);
         } else {
-            console.log(`card1Num`);
+            money = money - betMoney.value;
+            h2.innerText = `${money}`;
         }
     } else if(isNaN(parseInt(card2Num)) && card2Num!=="JOKER"){
-        console.log(`card2Num`);
-        money*=2;
+        money = money + parseInt(betMoney.value*highMoneyPer);
         h2.innerText = `${money}`;
     } else if(isNaN(parseInt(card1Num)) && card2Num!=="JOKER"){
-        console.log(`card1Num`);
+        money = money - betMoney.value;
+        h2.innerText = `${money}`;
     } else {
         if(parseInt(card1Num)<parseInt(card2Num)){
-            console.log(`card2Num`);
-            money*=2;
+            money = money + parseInt(betMoney.value*highMoneyPer);
             h2.innerText = `${money}`;
         } else if(card1Num===card2Num){
             console.log(`draw`);
         } else {
-            console.log(`card1Num`);
+            money = money - betMoney.value;
+            h2.innerText = `${money}`;
         }
     }
+
+    if(card2Num===11){
+        card2Num = ["J"];
+    }else if(card2Num===12){
+        card2Num = ["Q"];
+    }else if(card2Num===13){
+        card2Num = ["K"];
+    }else if(card2Num===14){
+        card2Num = ["A"];
+    }else if(card2Num===15){
+        card2Num = ["JOKER"];
+    }
+    
+    if(card1Num===11){
+        card1Num = ["J"];
+    }else if(card1Num===12){
+        card1Num = ["Q"];
+    }else if(card1Num===13){
+        card1Num = ["K"];
+    }else if(card1Num===14){
+        card1Num = ["A"];
+    }else if(card2Num===15){
+        card2Num = ["JOKER"];
+    }
+
+    //card2.innerText = `${card2Type}${card2Num}`;
+}
+
+
+function ifButtonClickIsDown(){
+    bettingButton.classList.add("hidden");
+
+    if(card2Num==="JOKER" && card1Num !== "JOKER"){
+        money = money + parseInt(betMoney.value*highMoneyPer);
+        h2.innerText = `${money}`;
+    }
+    if(card2Num==="JOKER" && card1Num === "JOKER"){
+        console.log(`draw`);
+    }
+    if(card2Num!=="JOKER" && card1Num === "JOKER"){
+        money = money - parseInt(betMoney.value);
+        h2.innerText = `${money}`;
+    }
+
+    if(isNaN(parseInt(card2Num)) && card2Num!=="JOKER" && isNaN(parseInt(card1Num)) && card1Num!=="JOKER"){
+        if((card1Num==="J" && card2Num==="Q") || (card1Num==="Q" && card2Num==="K") || (card1Num==="K" && card2Num==="A") || (card1Num==="Q" && card2Num==="A")){
+            money = money - betMoney.value;
+            h2.innerText = `${money}`;
+        } else if(card1Num===card2Num){
+            console.log(`draw`);
+        } else {
+            money = money + parseInt(betMoney.value*highMoneyPer);
+            h2.innerText = `${money}`;
+        }
+    } else if(isNaN(parseInt(card2Num)) && card2Num!=="JOKER"){
+        money = money - betMoney.value;
+        h2.innerText = `${money}`;
+    } else if(isNaN(parseInt(card1Num)) && card2Num!=="JOKER"){
+        money = money + parseInt(betMoney.value*highMoneyPer);
+        h2.innerText = `${money}`;
+    } else {
+        if(parseInt(card1Num)<parseInt(card2Num)){
+            money = money - betMoney.value;
+            h2.innerText = `${money}`;
+        } else if(card1Num===card2Num){
+            console.log(`draw`);
+        } else {
+            money = money + parseInt(betMoney.value*highMoneyPer);
+            h2.innerText = `${money}`;
+        }
+    }
+
+    if(card2Num===11){
+        card2Num = ["J"];
+    }else if(card2Num===12){
+        card2Num = ["Q"];
+    }else if(card2Num===13){
+        card2Num = ["K"];
+    }else if(card2Num===14){
+        card2Num = ["A"];
+    }else if(card2Num===15){
+        card2Num = ["JOKER"];
+    }
+
+    if(card1Num===11){
+        card1Num = ["J"];
+    }else if(card1Num===12){
+        card1Num = ["Q"];
+    }else if(card1Num===13){
+        card1Num = ["K"];
+    }else if(card1Num===14){
+        card1Num = ["A"];
+    }else if(card2Num===15){
+        card2Num = ["JOKER"];
+    }
+
+    //card2.innerText = `${card2Type}${card2Num}`;
+}
+
+function calculatorMoney(){
+    if(card1Num==="J"){
+        card1Num = 11;
+    } else if(card1Num==="Q"){
+        card1Num = 12;
+    } else if(card1Num==="K"){
+        card1Num = 13;
+    } else if(card1Num==="A"){
+        card1Num = 14;
+    } else if(card1Num==="JOKER"){
+        card1Num = 15;
+    }
+    if(card2Num==="J"){
+        card2Num = 11;
+    } else if(card2Num==="Q"){
+        card2Num = 12;
+    } else if(card2Num==="K"){
+        card2Num = 13;
+    } else if(card2Num==="A"){
+        card2Num = 14;
+    } else if(card2Num==="JOKER"){
+        card2Num = 15;
+    }
+    let downCount=0, upCount=0;
+    for(let i=1; i<parseInt(card1Num); i++){  //  LOW 계산
+        downCount++;
+    }
+    for(let i=parseInt(card1Num); i<15; i++){ // HIGH 계산
+        upCount++;
+    }
+    highMoneyPer = Math.round(((downCount*0.1)+1)*1000)/1000;
+    lowMoneyPer = Math.round(((upCount*0.1)+1)*1000)/1000;
+
+    upRange.innerText = `x${highMoneyPer}`;
+    downRange.innerText = ` x${lowMoneyPer}`;
 }
